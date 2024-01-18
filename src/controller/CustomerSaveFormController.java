6f08c7840245e713db066f154804a33b06c8deba
Ranger-HK <maxravindu99@gmail.com>
2024-01-18 17:36:22 +0530

    Validation Add For Customer Save Form

--- src/controller/CustomerSaveFormController.java
+++ src/controller/CustomerSaveFormController.java
@@ -4,9 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import model.Customer;
 
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 
 /**
  * @Created By Ravindu Prathibha
@@ -21,37 +24,37 @@ public class CustomerSaveFormController {
     public JFXTextField txtCustomerSalary;
     public JFXButton btnSaveCustomer;
 
+    public void initialize() {
+        btnSaveCustomer.setDisable(true);
+    }
+
     public void SaveCusOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-        Customer c1=new Customer(
+        Customer c1 = new Customer(
                 txtCustomerId.getText(),
                 txtCustomerName.getText(),
                 txtCustomerAddress.getText(),
                 Double.parseDouble(txtCustomerSalary.getText())
         );
 
-        if (new CustomerController().saveCustomer(c1)){
-                new Alert(Alert.AlertType.CONFIRMATION,"Customer Saved").show();
-                txtClear();
+        if (new CustomerController().saveCustomer(c1)) {
+            new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved").show();
+            txtClear();
 
-        }else {
-                new Alert(Alert.AlertType.WARNING,"Try Again").show();
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Try Again").show();
 
         }
 
     }
 
     public void moveNameOnAction(ActionEvent actionEvent) {
-        txtCustomerName.requestFocus();
-
     }
 
     public void moveAddresOnAction(ActionEvent actionEvent) {
-        txtCustomerAddress.requestFocus();
+
     }
 
     public void moveSalaryOnAction(ActionEvent actionEvent) {
-        txtCustomerSalary.requestFocus();
-
     }
 
     public void txtClear() {
@@ -59,5 +62,71 @@ public class CustomerSaveFormController {
         txtCustomerName.clear();
         txtCustomerAddress.clear();
         txtCustomerSalary.clear();
+    }
+
+
+    public void textFieldOnKeyReleased(KeyEvent keyEvent) {
+
+        String idRegEx = "^(C00-)[0-9]{3,4}$";
+        String nameRegEx = "^[A-z ]{4,12}$";
+        String addressRegEx = "^[A-z0-9/ ]{6,32}$";
+        String salaryRegEx = "^[1-9][0-9]*([.][0-9]{2})?$";
+
+        Pattern idPattern = Pattern.compile(idRegEx);
+        Pattern namePattern = Pattern.compile(nameRegEx);
+        Pattern addressPattern = Pattern.compile(addressRegEx);
+        Pattern salaryPattern = Pattern.compile(salaryRegEx);
+
+        String typeTextId = txtCustomerId.getText();
+        String typeTextName = txtCustomerName.getText();
+        String typeTextAddress = txtCustomerAddress.getText();
+        String typeTextSalary = txtCustomerSalary.getText();
+
+
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            if (idPattern.matcher(typeTextId).matches()) {
+                txtCustomerId.setStyle("-fx-background-color: green");
+                txtCustomerName.requestFocus();
+
+
+                if (namePattern.matcher(typeTextName).matches()) {
+                    txtCustomerName.setStyle("-fx-background-color: green");
+                    txtCustomerAddress.requestFocus();
+
+
+                    if (addressPattern.matcher(typeTextAddress).matches()) {
+                        txtCustomerAddress.setStyle("-fx-background-color: green");
+                        txtCustomerSalary.requestFocus();
+
+
+                        if (salaryPattern.matcher(typeTextSalary).matches()) {
+                            txtCustomerSalary.setStyle("-fx-background-color: green");
+                            btnSaveCustomer.requestFocus();
+
+
+                        } else {
+                            txtCustomerSalary.setStyle("-fx-background-color: red");
+                            txtCustomerSalary.requestFocus();
+                        }
+
+                    } else {
+                        txtCustomerAddress.setStyle("-fx-background-color: red");
+                        txtCustomerAddress.requestFocus();
+                    }
+
+
+                } else {
+                    txtCustomerName.setStyle("-fx-background-color: red");
+                    txtCustomerName.requestFocus();
+                }
+
+
+            } else {
+                txtCustomerId.setStyle("-fx-background-color: red");
+                txtCustomerId.requestFocus();
+
+            }
+        }
+
     }
 }
